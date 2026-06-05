d56890d889005e3c8389ceac3bc24b381530a899
Sudhanshu Koli <kolisudhanshu@gmail.com>
2026-06-05 23:38:57 +0530

    edited DB props

--- src/main/java/com/interviewpros/controller/AuthController.java
+++ src/main/java/com/interviewpros/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://interview-pro-by-sudhanshu.vercel.app"})
 public class AuthController {
 
 
