a0b73475a1cbf871c64893b5bb82401e829efb16
Sudhanshu Koli <kolisudhanshu@gmail.com>
2026-06-05 20:17:54 +0530

    Edited properties file

--- src/main/java/com/interviewpros/repository/UserProfileRepository.java
+++ src/main/java/com/interviewpros/repository/UserProfileRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+//public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+//
+//    Optional<UserProfile> findByUsername(String username);
+//
+//}
+
+
+public interface UserProfileRepository {
 
     Optional<UserProfile> findByUsername(String username);
 
